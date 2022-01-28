1699d99f4c63fba1a4232cf9fc733129afe07b69
newtratip <tharathip.chaweewongphan@gmail.com>
2022-01-28 15:32:05 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Tongthai Repository
---
+++ tongthai_install/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Tongthai: Install",
+    "summary": "Used to install other modules",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Tongthai",
+    "author": "Ecosoft",
+    "installable": True,
+    "depends": [
+        "sale_management",
+        "purchase",
+        "purchase_request",
+    ]
+}
